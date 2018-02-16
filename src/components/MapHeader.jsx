8af050a87d93ffd9b5ba0fc85ed930fefcc11af0
Kurtis Houser <houserkurtis@gmail.com>
2018-02-16 12:36:30 -0800

    Move header to separate component

---
+++ src/components/MapHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MapHeader.css';
+
+const MapHeader = () => {
+  return (
+    <header className="map-header">
+      <h1 className="map-title">Resiliency Map</h1>
+    </header>
+  );
+};
+
+export default MapHeader;
