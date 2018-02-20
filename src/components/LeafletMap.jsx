ece898d10606a449e30c207eae87e66f72ba75ea
Kurtis Houser <houserkurtis@gmail.com>
2018-02-20 09:21:20 -0800

    Add simple click handler

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -18,15 +18,49 @@ const mapCenter = [37.80, -122.42];
 const zoomLevel = 14;
 
 class LeafletMap extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      hasLocation: false,
+      latlng: mapCenter,
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({
+      hasLocation: true,
+      latlng: e.latlng,
+    })
+  }
+
   render() {
+    const { latlng } = this.state;
+    const { lat, lng } = this.state.latlng;
+
+    const marker = this.state.hasLocation ? (
+      <Marker position={latlng}>
+        <Popup>
+          <span>{lat}, {lng}</span>
+        </Popup>
+      </Marker>
+    ) : null;
+
     return (
-      <Map className="map" center={mapCenter} zoom={zoomLevel}>
+      <Map
+        className="map"
+        center={mapCenter}
+        zoom={zoomLevel}
+        onClick={this.handleClick}>
         <TileLayer attribution={attribution} url={osmTiles} />
-        <Marker position={mapCenter}>
+        {/* <Marker position={mapCenter}>
           <Popup>
             <span>Center of the Map</span>
           </Popup>
-        </Marker>
+        </Marker> */}
+        { marker }
       </Map>
     );
   }
